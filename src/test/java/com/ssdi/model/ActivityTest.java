524249cb6b514c59aa981b7c4e08eb1fe7b6b61a
dshah29 <dshah29@uncc.edu>
2018-04-13 03:03:57 -0400

    FirstCommit

--- src/test/java/com/ssdi/model/ActivityTest.java
+++ src/test/java/com/ssdi/model/ActivityTest.java
@@ -1,54 +1,55 @@
 package com.ssdi.model;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.ssdi.dao.ActivityDao;
 
 public class ActivityTest {
 
+	private static ActivityDao mockActivityDao;
+	private static Activity act1;
+	private static Activity act2;
+	
+	@Before
+	public void setup()
+	{
+		//Create mock Object of ActivityDao
+		mockActivityDao=mock(ActivityDao.class);
+		
+		
+		//Create Few instances of Activity Class
+		act1=new Activity();
+		act1.setActivity_name("Badminton");
+		act1.setId(1);
+		
+		act2= new Activity();
+		act2.setActivity_name("Volleyball");
+		act2.setId(2);
+		
+		//Mocking
+		when(mockActivityDao.findAll()).thenReturn(Arrays.asList(act1,act2));
+			
+	}	
+	
+	Activity act;
+	List<Activity> allActivities;
+	
 	@Test
-	public void testActivityString() {
-		fail("Not yet implemented");
-	}
+	public void testGetActivityName() 
+	{
+		allActivities = mockActivityDao.findAll();
+		assertEquals(2,allActivities.size());
+		act = allActivities.get(0);
+		assertEquals("Badminton",act.getActivity_name());
+		assertEquals(1,act.getId());
 
-	@Test
-	public void testGetActivityId() {
-		fail("Not yet implemented");
 	}
-
-	@Test
-	public void testSetActivityId() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetActivityName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetActivityName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetVenue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetVenue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testActivity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testActivityStringInt() {
-		fail("Not yet implemented");
-	}
-
 }
